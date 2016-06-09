194dab7f3a1dd4405558aafacdb3161190cdfcea
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2016-06-09 12:46:53 +0200

    comment

--- package.lisp
+++ package.lisp
@@ -1,6 +1,8 @@
 ;;; package.lisp
 ;;;; link-manager.lisp
 ;;;; depends on Hunchentoot
+;;;; TODO; implement search, search for term
+;;;; (or (member term title) (member term tags) (member term summary)
 
 (defpackage :link-manager
   (:use :cl :hunchentoot :cl-who))
