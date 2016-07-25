863f17fc2f2fef02f83befdfb62992540392efe3
jeroen.peters <jeroen.peters@localhost>
2016-07-25 10:19:22 +0200

    Show the title

--- javascript/confirmdelete.js
+++ javascript/confirmdelete.js
@@ -2,6 +2,6 @@ $(document).ready(function()
 {
     $('.confirm-delete').click(function()
     {
-        return confirm('Are you sure you want to delete "' + $(this).attr('title') + '" from the Link Manager?');
+        return confirm('Are you sure you want to delete "' + $(this).attr('data-title') + '" from the Link Manager?');
     });
 });
