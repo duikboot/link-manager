6442beea2d0f251185c185fe5f12eae72232c0f0
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2016-11-22 18:12:22 +0100

    better indentation

--- web.lisp
+++ web.lisp
@@ -8,11 +8,11 @@
             (make-instance 'hunchentoot:easy-acceptor :port *web-port*)))))
 
 (defun stop-server ()
- (if (not *web-acceptor*)
-  (format t "~%Webserver not running~%")
-  (progn
-    (hunchentoot:stop *web-acceptor*)
-    (setf *web-acceptor* nil))))
+  (if (not *web-acceptor*)
+    (format t "~%Webserver not running~%")
+    (progn
+      (hunchentoot:stop *web-acceptor*)
+      (setf *web-acceptor* nil))))
 
 (defun get-order (x)
   (if (equalp "desc" x) #'> #'<))
@@ -27,52 +27,60 @@
 
 ;standard page for all the html pages
 (defmacro standard-page ((&key title) &body body)
-  `(with-html-output-to-string (*standard-output* nil :prologue t :indent t)
+  `(with-html-output-to-string
+     (*standard-output* nil :prologue t :indent t)
      (:html
-      (:head
-       (:meta :content "text/html" :charset "utf-8")
-       (:meta :http-equiv "X-UA-Compatible" :content "IE=edge")
-       (:meta :name "viewport" :content "width=device-width, initial-scale=1")
-       (:title ,title)
-       (:link :href "/style.css" :rel "stylesheet" :type "text/css")
-       (:link :href "/bootstrap.css" :rel "stylesheet" :type "text/css")
-       (:link :href "/font-awesome.css" :rel "stylesheet" :type "text/css")
-       (:script :src "/jquery.js")
-       (:script :src "/bootstrap-js.js")
-       (:script :src "/confirmdelete.js"))
-      (:body
-        (header)
-        (:div :style "min-height:450px"
-         (:h2 :style "text-align:center" ,title)
-         (:br)
-         ,@body)
-        (footer)))))
+       (:head
+         (:meta :content "text/html" :charset "utf-8")
+         (:meta :http-equiv "X-UA-Compatible" :content "IE=edge")
+         (:meta :name "viewport" :content "width=device-width, initial-scale=1")
+         (:title ,title)
+         (:link :href "/style.css" :rel "stylesheet" :type "text/css")
+         (:link :href "/bootstrap.css" :rel "stylesheet" :type "text/css")
+         (:link :href "/font-awesome.css" :rel "stylesheet" :type "text/css")
+         (:script :src "/jquery.js")
+         (:script :src "/bootstrap-js.js")
+         (:script :src "/confirmdelete.js"))
+       (:body
+         (header)
+         (:div :style "min-height:450px"
+               (:h2 :style "text-align:center" ,title)
+               (:br)
+               ,@body)
+         (footer)))))
 
 ;; header procedure
 (defun header ()
-  (with-html-output (*standard-output* nil :indent t)
-    (:nav :class "navbar navbar-inverse navbar-fixed-top"
+  (with-html-output
+    (*standard-output* nil :indent t)
+    (:nav
+      :class "navbar navbar-inverse navbar-fixed-top"
       (:div :class "container-fluid"
-        (:div :class "navbar-header"
-          (:a :class "navbar-brand" :href "/"
-            (:i :class "glyphicon glyphicon-home"))
-          (:ul :class "nav navbar-nav"
-            (:li
-                (:a :href "/bookmarks/add" "Add bookmark")))
-          (:form :method "get" :action "/bookmarks/" :class "navbar-form navbar-left" :role "search"
-            (:div :class "form-group"
-              (:input :type "text" :name "search"
-                      :class "form-control" :placeholder "Search bookmarks..."))))))))
+            (:div :class "navbar-header"
+                  (:a :class "navbar-brand" :href "/"
+                      (:i :class "glyphicon glyphicon-home"))
+                  (:ul :class "nav navbar-nav"
+                       (:li
+                         (:a :href "/bookmarks/add" "Add bookmark")))
+                  (:form :method "get"
+                         :action "/bookmarks/"
+                         :class "navbar-form navbar-left" :role "search"
+                         (:div :class "form-group"
+                               (:input :type "text" :name "search"
+                                       :class "form-control"
+                                       :placeholder "Search bookmarks..."))))))))
 
  ;; footer content
 (defun footer ()
-  (with-html-output (*standard-output* nil :indent t)
-   (:footer :class "footer-distributed"
-     (:div :class "footer-right"
-       (:a :href "https://twitter.com/u_boot" (:i :class "fa fa-twitter")))
-     (:div :class "footer-left"
-      (:a :href "/index" (:img  :class "logo" :src "/images/logo.png"))
-      (:p :style "color: black;" "Duikboot &copy; 2016")))))
+  (with-html-output
+    (*standard-output* nil :indent t)
+    (:footer :class "footer-distributed"
+             (:div :class "footer-right"
+                   (:a :href "https://twitter.com/u_boot"
+                       (:i :class "fa fa-twitter")))
+             (:div :class "footer-left"
+                   (:a :href "/index" (:img  :class "logo" :src "/images/logo.png"))
+                   (:p :style "color: black;" "Duikboot &copy; 2016")))))
 
 (defun index ()
   (redirect "/bookmarks/"))
@@ -126,42 +134,56 @@
         :class "alert alert-success fade in" "Bookmark successfully added."))))
 
 (defun bookmark-form (&key id title link summary tags (error nil))
-    (standard-page
-      (:title "Add bookmark")
-      (:div :style "margin: 0 auto; width: 90%"
-            (:form :method "post" :class "form-horizontal" :action "/bookmarks/save"
-                   (:input :type "hidden" :value (if id id 0) :name "id")
-                   (when error
-                     (show-error))
-                   (:div :class "form-group"
-                         (:label :for "bf_title" :class "col-sm-2 control-label" "Title")
-                         (:div :class "col-sm-10"
-                             (:input :type "text" :id "bf_title" :placeholder "Bookmark title"
-                                :tabindex "1" :class "form-control" :value
-                                 (if title (format nil "~{~(~a~^ ~)~}" title) nil) :name "title")))
-                   (:div :class "form-group"
-                         (:label :for "bf_link" :class "col-sm-2 control-label" "Link")
-                         (:div :class "col-sm-10"
-                             (:input :type "url" :id "bf_link" :placeholder "https://www.site.com"
-                                :tabindex "2" :class "form-control" :value
-                                 (if link link nil) :name "link")))
-                   (:div :class "form-group"
-                         (:label :for "bf_sum" :class "col-sm-2 control-label" "Summary")
-                         (:div :class "col-sm-10"
-                            (format t "<textarea name=\"summary\" placeholder=\"Descriptive summary of site contents\"
-                                tabindex=\"3\" class=\"form-control\" rows=\"10\" cols=\"70\">")
-                            (if summary (format t "~{~(~a~^ ~)~}" summary))
-                            (format t "</textarea>")))
-                   (:div :class "form-group"
-                         (:label :for "bf_tags" :class "col-sm-2 control-label" "Tags")
-                         (:div :class "col-sm-10"
-                             (:input :type "text" :id "bf_tags" :placeholder "space separated words"
-                                :tabindex "4" :class "form-control" :value
-                                 (if tags (format nil "~{~(~a~^ ~)~}" tags) nil) :name "tags" :size 80)))
-                   (:div :class "form-group"
-                        (:div :class "col-sm-offset-2 col-sm-10"
-                            (:button :type "submit" :tabindex "5" :class "btn btn-primary" "Save")
-                            (:a :tabindex "6" :href "/bookmarks/" :class "btn" "Cancel")))))))
+  (standard-page
+    (:title "Add bookmark")
+    (:div :style "margin: 0 auto; width: 90%"
+          (:form :method "post" :class "form-horizontal" :action "/bookmarks/save"
+                 (:input :type "hidden" :value (if id id 0) :name "id")
+                 (when error
+                   (show-error))
+                 (:div :class "form-group"
+                       (:label :for "bf_title"
+                               :class "col-sm-2 control-label" "Title")
+                       (:div :class "col-sm-10"
+                             (:input
+                               :type "text" :id "bf_title"
+                               :placeholder "Bookmark title"
+                               :tabindex "1" :class "form-control" :value
+                               (if title
+                                 (format nil "~{~(~a~^ ~)~}" title)
+                                 nil)
+                               :name "title")))
+                 (:div :class "form-group"
+                       (:label
+                         :for "bf_link"
+                         :class "col-sm-2 control-label" "Link")
+                       (:div :class "col-sm-10"
+                             (:input
+                               :type "url"
+                               :id "bf_link"
+                               :placeholder "https://www.site.com"
+                               :tabindex "2" :class "form-control" :value
+                               (if link link nil)
+                               :name "link")))
+                 (:div :class "form-group"
+                       (:label
+                         :for "bf_sum"
+                         :class "col-sm-2 control-label" "Summary")
+                       (:div :class "col-sm-10"
+                             (format t "<textarea name=\"summary\" placeholder=\"Descriptive summary of site contents\"
+                                     tabindex=\"3\" class=\"form-control\" rows=\"10\" cols=\"70\">")
+                             (if summary (format t "~{~(~a~^ ~)~}" summary))
+                             (format t "</textarea>"
+                                     (:div :class "form-group"
+                                           (:label :for "bf_tags" :class "col-sm-2 control-label" "Tags")
+                                           (:div :class "col-sm-10"
+                                                 (:input :type "text" :id "bf_tags" :placeholder "space separated words"
+                                                         :tabindex "4" :class "form-control" :value
+                                                         (if tags (format nil "~{~(~a~^ ~)~}" tags) nil) :name "tags" :size 80)))
+                                     (:div :class "form-group"
+                                           (:div :class "col-sm-offset-2 col-sm-10"
+                                                 (:button :type "submit" :tabindex "5" :class "btn btn-primary" "Save")
+                                                 (:a :tabindex "6" :href "/bookmarks/" :class "btn" "Cancel"))))))))))
 
 
 (defun render-tags (tags tags-list)
